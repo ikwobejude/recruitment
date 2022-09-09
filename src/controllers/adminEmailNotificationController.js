a1917aff0c9e8e92103b1dc7992949ef7e77486a
ikwobejude <ikwobejude@gmail.com>
2022-09-09 12:25:05 +0100

    Updates

--- src/controllers/adminEmailNotificationController.js
+++ src/controllers/adminEmailNotificationController.js
@@ -1,9 +1,12 @@
+const model = require("../model/applicant")
 
 
 
 module.exports.adminSendEmailNotificationGet = async(req, res) => {
+    let ministies = await model.organization.findAll();
     res.render('./admin/admin_send_email', {
-        title: "Emailing"
+        title: "Emailing", 
+        ministies
     })
 }
 
